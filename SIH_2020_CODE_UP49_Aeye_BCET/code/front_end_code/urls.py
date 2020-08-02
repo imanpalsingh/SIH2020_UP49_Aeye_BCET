e9206f216928635384d0b87a9647d20d9fa5476d
9455136334 <50235856+9455136334@users.noreply.github.com>
2020-08-02 10:26:25 +0530

    created information pages

--- SIH_2020_CODE_UP49_Aeye_BCET/code/front_end_code/urls.py
+++ SIH_2020_CODE_UP49_Aeye_BCET/code/front_end_code/urls.py
@@ -14,16 +14,30 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import url
 from front_end_main_app import views
 from django.urls import path,include
 from django.conf.urls import url
-from front_end_main_app import views
+from django.views.generic.base import TemplateView
+
 
 urlpatterns = [
 
     url(r'^$',views.index,name='index'),
     path('accounts/',include('django.contrib.auth.urls')),
     url(r'^input_form',views.patientForm,name='input_form'),
+    url(r'^information/', TemplateView.as_view(template_name='information.html'), name='Information_page'),
+    url(r'^what_is_sepsis', TemplateView.as_view(template_name='what_is_sepsis.html'),
+        name='Information_page_what_is_sepsis'),
+    url(r'^how_can_i_get_ahead_of_sepsis', TemplateView.as_view(template_name='get_ahead_from_sepsis.html'),
+        name='Information_page_get_ahead_of_sepsis'),
+    url(r'^sepsis_diagnosed_and_treatment', TemplateView.as_view(template_name='sepsis_treatment.html'),
+        name='Information_page_sepsis_treatment'),
+    url(r'^educational_info_pt', TemplateView.as_view(template_name='educational_info1.html'),
+        name='Information_page_educational_info1'),
+    url(r'^educational_info_pro', TemplateView.as_view(template_name='educational_info2.html'),
+        name='Information_page_educational_info2'),
+    url(r'^sepsis_data_and_reports', TemplateView.as_view(template_name='sepsis_data_and_reports.html'),
+        name='Information_page_sepsis_data_and_reports'),
+
     path('admin/', admin.site.urls),
 ]
